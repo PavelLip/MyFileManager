4ec59c226858057782416dfa92db38e31ac823bc
Pavel <zavod93@mail.ru>
2021-05-01 20:33:20 +0300

    t1

--- FileManager/FileManager/Program.cs
+++ FileManager/FileManager/Program.cs
@@ -11,6 +11,9 @@ namespace FileManager
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world");
         }
     }
 }
